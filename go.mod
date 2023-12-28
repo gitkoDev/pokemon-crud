e6390738def61620cbcbc389a8c1526c6277b320
gitkoDev <prostodenis1397@gmail.com>
2023-12-28 10:55:46 +0800

    add env variable PORT and getEnv fuction

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gitkoDev/pokemon-crud.git
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Pokemon struct {
 	Name string `json:"name"`
 	Type []PokemonType
@@ -29,5 +37,24 @@ const (
 )
 
 func main() {
+	port := getEnv("PORT")
 
+	fmt.Println("running server on port", port)
+
+	http.ListenAndServe(":"+port, nil)
+}
+
+func getEnv(v string) string {
+	err := godotenv.Load(".env")
+
+	// Catch errors while opening the ENV file
+	if err != nil {
+		return fmt.Sprintln(err)
+	} else {
+		// Catch errors if the ENV variable is not found
+		if _, ok := os.LookupEnv(v); !ok {
+			return fmt.Sprintf("env variable %s not found", v)
+		}
+		return os.Getenv(v)
+	}
 }
